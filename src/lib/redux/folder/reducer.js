ba981e2387cdb9449294f4ac116d7c79063479af
Jiseung Roh <jiseung.roh@gmail.com>
2018-02-23 20:27:41 +0900

    Add a defaultState for folders

--- src/lib/redux/folder/reducer.js
+++ src/lib/redux/folder/reducer.js
@@ -2,8 +2,15 @@ import { actionTypes } from './actions'
 import uuid from 'uuid'
 
 // typeof folder state = array
+const defaultState = [
+  {
+    id: uuid.v4(),
+    name: '모든 노트',
+    createdAt: Date.now()
+  }
+]
 
-const reducer = (state = [], action) => {
+const reducer = (state = defaultState, action) => {
   switch (action.type) {
     case actionTypes.ADD_FOLDER:
       const folderId = uuid.v4()
