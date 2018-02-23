46583bacab94d830edcd09fe45d252d614b9fe10
Jiseung Roh <jiseung.roh@gmail.com>
2018-02-23 21:07:58 +0900

    Make NoteList work (test)

--- src/components/notes/NoteList.js
+++ src/components/notes/NoteList.js
@@ -6,40 +6,72 @@ class NoteListItem extends React.Component {
     const {
       title,
       content,
-      date,
-      isActive,
+      createdAt,
     } = this.props
 
     return (
-      <li
-        className={`list-item${isActive ? ' active' : ''}`}
-        onClick={this.handleClick}
-      >
+      <li className='list-item'>
         <h1>{title}</h1>
         <p className='content'>{content}</p>
-        <p className='date'>{date}</p>
+        <p className='date'>{createdAt}</p>
       </li>
     )
   }
 }
 
 class NoteList extends React.Component {
+  createNote = (e) => {
+    e.preventDefault()
+
+    const {
+      actions,
+      route,
+    } = this.props
+
+  }
+
   render () {
+    const {
+      data
+    } = this.props
+
     return (
       <section className='note-list'>
         <header className='note-header'>
-          <h1>
-            <h1>나는 노트리스트입니다</h1>
+          <h1 className='sr-only'>
+            노트 리스트
           </h1>
         </header>
         <ul className='list'>
           <li className='add-btn'>
             <button
               type='button'
+              onClick={this.createNote}
             >
               새 글 작성하기
             </button>
           </li>
+          {
+            Object
+              .entries(data)
+              .map((item, index) => {
+                console.log('data:', data)
+                console.log('item:', item)
+                const {
+                  noteId,
+                  title,
+                  content,
+                  createdAt,
+                } = item[1]
+
+                return <NoteListItem
+                  key={noteId}
+                  title={title}
+                  content={content}
+                  createdAt={createdAt}
+                />
+              })
+          }
         </ul>
       </section>
     )
